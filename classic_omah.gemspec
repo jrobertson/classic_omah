fbe16565b27723c95a8eb433af6491a8895e93f5
James Robertson <james@jamesrobertson.eu>
2018-09-26 19:46:28 +0100

    minor improvement: Added the debug keyword to make it easier to debug.

--- classic_omah.gemspec
+++ classic_omah.gemspec
@@ -1,14 +1,14 @@
 Gem::Specification.new do |s|
   s.name = 'classic_omah'
-  s.version = '0.2.5'
+  s.version = '0.3.0'
   s.summary = 'Mail gem + Omah (Offline Mail Helper) gem'
   s.authors = ['James Robertson']
   s.files = Dir['lib/classic_omah.rb']
-  s.add_runtime_dependency('omah', '~> 0.7', '>=0.7.0')
-  s.add_runtime_dependency('mail', '~> 2.6', '>=2.6.3')
+  s.add_runtime_dependency('omah', '~> 0.9', '>=0.9.1')
+  s.add_runtime_dependency('mail', '~> 2.7', '>=2.7.0')
   s.signing_key = '../privatekeys/classic_omah.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
-  s.email = 'james@r0bertson.co.uk'
+  s.email = 'james@jamesrobertson.eu'
   s.homepage = 'https://github.com/jrobertson/classic_omah'
 end
--- lib/classic_omah.rb
+++ lib/classic_omah.rb
@@ -11,9 +11,12 @@ end
 
 class ClassicOmah < Omah
 
-  def initialize(user: 'user', filepath: '.', mail: {}, port: 110, \
-               email_address: nil, options: {xslt: 'listing.xsl'}, plugins: [])
+  def initialize(user: 'user', filepath: '.', mail: {}, port: 110, 
+                 email_address: nil, options: {xslt: 'listing.xsl'}, 
+                 plugins: [], debug: false)
         
+    puts 'inside ClassicOmah::initialize' if debug        
+    
     @mail = {    address: '',
                     port: port,
                user_name: '',
@@ -27,26 +30,40 @@ class ClassicOmah < Omah
     @variables = {user_name: @mail[:user_name] , \
                   address: @mail[:address], email_address: email_address}
     
-    super(user: user, filepath: filepath, plugins: plugins, options: options)    
+    super(user: user, filepath: filepath, plugins: plugins, 
+          options: options, debug: debug)
+    
+    puts 'initialize ClassicOmah complete' if @debug
     
     
   end
 
   def fetch_email()
     
+    puts 'inside fetch_email' if @debug
     mail = @mail
+    puts 'mail: ' + mail.inspect if @debug
+    
     Mail.defaults { retriever_method(:pop3, mail) }
-
+    
+    puts 'before Mail.all' if @debug
     email = Mail.all
+    puts 'after Mail.all' if @debug
 
     return 'no new messages' unless email.any?
+    
+    puts 'before email.map' if @debug
 
     messages = email.map.with_index.inject([]) do |r, x|
       
+      
+      puts 'x: ' + x.inspect if @debug
+            
       msg, i = x
 
+
       begin
-        r << {
+        r << [x, {
           msg_id:     msg.message_id,
           from:       msg.from.is_a?(Array) ? msg.from.join(', ') : msg.from,
           to:         msg.to.is_a?(Array) ? msg.to.join(', ') : msg.to,
@@ -56,7 +73,7 @@ class ClassicOmah < Omah
           body_html:  (msg.html_part ? msg.html_part.body.decoded : msg.body), 
           attachments: msg.attachments.map {|x| [x.filename, x.body.decoded] },
           raw_source: msg.raw_source
-        }
+        }]
       rescue
         puts 'warning: ' + ($!).inspect
       end
